9f218caf05dfd1e252f57abfa2a419dc24b40b56
Eric Vogt <eric.lvogt@gmail.com>
2023-01-29 10:45:06 -0700

    Moon pass in classname through props

--- src/assets/moon.jsx
+++ src/assets/moon.jsx
@@ -2,7 +2,7 @@ export default function Moon(props) {
   return (
     <svg
       id="theme-toggle-dark-icon"
-      className="w-5 h-5 fill-notehub-tertiary"
+      className={props.className}
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
